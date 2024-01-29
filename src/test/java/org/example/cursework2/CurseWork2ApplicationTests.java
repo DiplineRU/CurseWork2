d7d74ed5512b1e4b8f21dd3e888f75fdba5d3cf6
DiplineRU <artgul52@gmail.com>
2024-01-29 17:48:08 +0300

    Intial commit

---
+++ src/test/java/org/example/cursework2/CurseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurseWork2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
